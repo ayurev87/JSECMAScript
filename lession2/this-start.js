729bd1f62a3acb24377d5232354cd05941a45e79
ayurev87 <a-yurev_87@mail.ru>
2023-08-11 07:09:52 +0300

    add lession2

---
+++ lession2/this-start.js
@@ -0,0 +1,23 @@
+// Работа с this
+const checkThis = function() {
+console.log(this);
+}
+checkThis(); // Window {0: global, window: Window, self: Window, document: document, name: "", location: Location, …}
+
+///////
+
+const checkThisInObject = {
+testProperty: true,
+checkThis: function () {
+console.log(this);
+},
+};
+checkThisInObject.checkThis(); // {testProperty: true, checkThis: ƒ}
+
+const checkThisInObject = {
+testProperty: true,
+checkThis: function () {
+console.log(this);
+},
+};
+checkThisInObject.checkThis(); // {testProperty: true, checkThis: ƒ}
